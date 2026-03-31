47edf79afac0ee292e250f8f0371bf036f2396dd
mousam <mousampandey61@gmail.com>
2026-04-01 01:17:01 +0530

    loading issue fix 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/H3-Epicura/',   // 👈 VERY IMPORTANT
+  base: '/',   // 👈 VERY IMPORTANT
 })
